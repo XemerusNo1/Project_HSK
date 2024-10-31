50ec76ff6c413a3edf2ce6432da352df83b5c592
BaoHocHoi <144047904+baovr125@users.noreply.github.com>
2024-10-31 08:26:59 +0700

    Add files via upload

---
+++ QuanLyRapPhim1/src/Entity/NhanVien.java
@@ -0,0 +1,59 @@
+package Entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class NhanVien {
+	private String maNV, tenNV;
+	private LocalDate namSinh;
+	private boolean gioiTinh;
+	public NhanVien(String maNV, String tenNV, LocalDate namSinh, boolean gioiTinh) {
+		super();
+		this.maNV = maNV;
+		this.tenNV = tenNV;
+		this.namSinh = namSinh;
+		this.gioiTinh = gioiTinh;
+	}
+	public String getMaNV() {
+		return maNV;
+	}
+	public void setMaNV(String maNV) {
+		this.maNV = maNV;
+	}
+	public String getTenNV() {
+		return tenNV;
+	}
+	public void setTenNV(String tenNV) {
+		this.tenNV = tenNV;
+	}
+	public LocalDate getNamSinh() {
+		return namSinh;
+	}
+	public void setNamSinh(LocalDate namSinh) {
+		this.namSinh = namSinh;
+	}
+	public boolean isGioiTinh() {
+		return gioiTinh;
+	}
+	public void setGioiTinh(boolean gioiTinh) {
+		this.gioiTinh = gioiTinh;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(maNV);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NhanVien other = (NhanVien) obj;
+		return Objects.equals(maNV, other.maNV);
+	}
+	
+	
+	
+}
